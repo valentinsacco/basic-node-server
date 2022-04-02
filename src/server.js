44a4605c28b957e9f2330dd6b14482a2d819a800
valentinsacco <vasacco@hotmail.com.ar>
2022-04-02 13:05:14 -0300

    compressing response with compression library

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import compression from 'compression'
 import morgan from 'morgan'
 import { config } from 'dotenv'
 
@@ -12,6 +13,7 @@ app.set('PORT', process.env.PORT || 4200)
 
 app.use(express.json())
 app.use(express.text())
+app.use(compression())
 app.use(morgan('dev'))
 
 app.use('/api', userRoute)
